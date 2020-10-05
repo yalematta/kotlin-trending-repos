9ccb5ca2f328cd97cc0fe4bf0de9269a0cafd511
yalematta <layale.matta@neo.ae>
2020-10-05 18:31:25 +0300

    Add loader + SearchAutoComplete

--- app/src/main/java/com/yalematta/trendingrepos/ui/repos/ReposFragment.kt
+++ app/src/main/java/com/yalematta/trendingrepos/ui/repos/ReposFragment.kt
@@ -1,18 +1,25 @@
 package com.yalematta.trendingrepos.ui.repos
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.SearchAutoComplete
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.paging.LoadState
 import com.google.android.material.transition.MaterialElevationScale
 import com.yalematta.trendingrepos.R
+import com.yalematta.trendingrepos.data.model.Languages
 import com.yalematta.trendingrepos.data.model.Repo
 import com.yalematta.trendingrepos.databinding.FragmentReposBinding
 import com.yalematta.trendingrepos.ui.repos.adapter.RepoClickListener
@@ -50,15 +57,39 @@ class ReposFragment : Fragment(R.layout.fragment_repos), RepoClickListener {
         binding.apply {
 
             recycler.setHasFixedSize(true)
-
+            recycler.itemAnimator = null
             recycler.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = ReposLoadStateAdapter { adapter.retry() },
                 footer = ReposLoadStateAdapter { adapter.retry() }
             )
+
+            btnRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         viewModel.repos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progress.isVisible = loadState.source.refresh is LoadState.Loading
+                recycler.isVisible = loadState.source.refresh is LoadState.NotLoading
+                btnRetry.isVisible = loadState.source.refresh is LoadState.Error
+                error.isVisible = loadState.source.refresh is LoadState.Error
+
+                // no results found
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    adapter.itemCount < 1
+                ) {
+                    recycler.isVisible = false
+                    emptyTv.isVisible = true
+                } else {
+                    emptyTv.isVisible = false
+                }
+            }
         }
 
         setHasOptionsMenu(true)
@@ -71,28 +102,34 @@ class ReposFragment : Fragment(R.layout.fragment_repos), RepoClickListener {
 
         val searchItem = menu.findItem(R.id.action_search)
         val searchView = searchItem.actionView as SearchView
+        val searchAutoComplete: SearchAutoComplete = searchView.findViewById(R.id.search_src_text)
 
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query != null) {
-                    binding.recycler.scrollToPosition(0)
-                    val languageQuery = String.format(getString(R.string.query), query)
-                    viewModel.searchRepos(languageQuery)
-                    searchView.clearFocus()
-                    (activity as AppCompatActivity).supportActionBar?.title =
-                        String.format(
-                            getString(R.string.language_name),
-                            query.capitalize(Locale.ROOT)
-                        )
-                }
-                return true
+        // Get SearchView autocomplete object.
+        searchAutoComplete.setTextColor(Color.WHITE)
+        searchAutoComplete.setDropDownBackgroundResource(R.color.colorPrimary)
+
+        val newsAdapter: ArrayAdapter<String> = ArrayAdapter(
+            this.requireContext(),
+            R.layout.dropdown_item,
+            Languages.data
+        )
+        searchAutoComplete.setAdapter(newsAdapter)
+
+        // Listen to search view item on click event.
+        searchAutoComplete.onItemClickListener =
+            OnItemClickListener { adapterView, _, itemIndex, _ ->
+                val queryString = adapterView.getItemAtPosition(itemIndex) as String
+                searchAutoComplete.setText(String.format(getString(R.string.search_query), queryString))
+                binding.recycler.scrollToPosition(0)
+                val languageQuery = String.format(getString(R.string.query), queryString)
+                viewModel.searchRepos(languageQuery)
+                searchView.clearFocus()
+                (activity as AppCompatActivity).supportActionBar?.title =
+                    String.format(
+                        getString(R.string.language_name),
+                        queryString.capitalize(Locale.ROOT)
+                    )
             }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return true
-            }
-
-        })
     }
 
     override fun onDestroyView() {
@@ -109,6 +146,7 @@ class ReposFragment : Fragment(R.layout.fragment_repos), RepoClickListener {
     private fun showDetails(repo: Repo, view: View) {
         val bundle = bundleOf("repo" to repo)
         val extras = FragmentNavigatorExtras(avatar to "userAvatar")
-        Navigation.findNavController(view).navigate(R.id.action_repos_to_details, bundle, null, extras )
+        Navigation.findNavController(view)
+            .navigate(R.id.action_repos_to_details, bundle, null, extras)
     }
 }
